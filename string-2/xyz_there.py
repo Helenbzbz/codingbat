64d7fbfdf65ccefe25cc18337a8fd9868d8fe2df
Helenbzbz <97910472+Helenbzbz@users.noreply.github.com>
2022-04-27 12:47:13 -0400

    update

--- string-2/xyz_there.py
+++ string-2/xyz_there.py
@@ -1,9 +1,11 @@
 def xyz_there(str):
-  for i in range(len(str)-3):
-    if i == 0:
-      print(str[i:i+3])
-      return str[i:i+3] == 'xyz' 
-    else:
-      a = str[i:i+3]
-      b = str[i-1:i+3]
-    return a == "xyz" and b != ".xyz"
+  for i in range(len(str)-2):
+    if i == 0 and str[i:i+3] == 'xyz':
+      return True
+
+    if i > 0:
+      if (str[i:i+3]) == 'xyz' and str[i-1:i+3] != '.xyz':
+        return True
+  return False
+  
+xyz_there('abcxyz')
